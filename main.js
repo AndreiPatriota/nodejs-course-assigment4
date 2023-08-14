1f9c39840dcf9bfed8c9e303c31d5faa3aac02e8
Andrei Patriota <63430047+AndreiPatriota@users.noreply.github.com>
2023-08-13 22:19:06 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const userNames = [];
+const port = 3853;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('users-add', { title: 'Add User' });
+});
+
+app.post('/users-add', (req, res) => {
+  const userName = req.body.name;
+  console.log(userName);
+  userNames.push(userName);
+  res.redirect('/');
+});
+
+app.get('/users', (req, res) => {
+  res.render('users', { title: 'List of Users', userNames: userNames });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
